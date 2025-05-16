566f132e223f621d2a7efda6cd91e75fbbe16625
Thomas Wu <thomaswu.wlh@gamil.com>
2025-05-16 11:38:06 +0800

    add image path of demo

--- yolov11_seg_test.py
+++ yolov11_seg_test.py
@@ -3,7 +3,7 @@ import numpy as np
 from ultralytics import YOLO
 
 # Load the YOLOv11-seg model (replace with the correct path/model name if needed)
-model = YOLO('yolo11n-seg.pt')
+model = YOLO('./weights/olo11n-seg.pt')
 
 # Ensure CUDA is available and use GPU
 if torch.cuda.is_available():
@@ -12,7 +12,7 @@ else:
     print("CUDA is not available. The model will run on CPU.")
 
 # Run segmentation inference on an image
-results = model(source='DJI_20250504171801_0005_V.JPG',device="cuda:0",classes=[0], retina_masks=True) # Replace 'input.jpg' with your image path
+results = model(source='./media/DJI_20250504171801_0005_V.JPG',device="cuda:0",classes=[0], retina_masks=True) # Replace 'input.jpg' with your image path
 
 result_masks = []
 masks = []
@@ -26,6 +26,6 @@ for result in results:
         masks.append(mask_np)
     result_masks.append(np.maximum.reduce(masks))  # Combine masks if multiple exist
     cv2.imshow('Mask', result_masks[-1])  # Display the mask
-    cv2.imwrite('DJI_20250504171801_0005_V_MASK.JPG', result_masks[-1])
+    cv2.imwrite('./media/DJI_20250504171801_0005_V_MASK.JPG', result_masks[-1])
     cv2.waitKey(0)  # Wait for a key press to close the window
 cv2.destroyAllWindows()
